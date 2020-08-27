0f81414dea69bafc3fc87de544dfd5fa6c3bed40
Stephanie Raymos <stephanieraymos@gmail.com>
2020-08-26 19:34:47 -0700

    Routing finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Nav from './Components/nav'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import PageRenderer from './page-renderer'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
+        <Switch>
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route component={() => 404} />
+        </Switch>
       </div>
     </Router>
   );
