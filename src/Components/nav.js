51470e354e3db57edb9d79f3e60aa9ef90f84d46
Stephanie Raymos <stephanieraymos@gmail.com>
2020-08-26 17:55:08 -0700

    Nav built

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,13 +1,34 @@
 import React from 'react'
 
+const navLinks = [
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'Blog',
+    path: '/blog'
+  },
+  {
+    title: 'Contact',
+    path: '/contact'
+  },
+  {
+    title: 'Login',
+    path: '/login'
+  }
+]
+
 export default function Nav() {
   return (
     <nav className="site-nav">
       <span>Blog</span>
       <ul>
-        <li>
-          
-        </li>
+        { navLinks.map((link, index) => (
+            <li key={index}>
+              {link.title}
+            </li>
+        ))}
       </ul>
 
     </nav>)
