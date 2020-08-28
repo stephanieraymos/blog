8186fbc685ef83a5f113b145e7bf8b158ff3ec73
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-08-28 15:28:50 -0700

    Setting up Home

---
+++ src/Components/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import trending from '../assets/mocks/trending'
+import {PostMasonry} from '../Components/common'
+
+export default function Home () {
+  return (
+  <section className="container home">
+      <div className="row">
+          <h2>Trending Posts</h2>
+
+          <PostMasonry posts={trending} columns={3} />
+      </div>
+
+  </section>
+  )
+}
