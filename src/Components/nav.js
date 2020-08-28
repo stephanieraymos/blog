0e27a1cddc7a7ac54a6c968a0ceca67fa864ce4b
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-08-27 22:57:12 -0700

    Fixed menu bar position

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Avatar } from "antd";
 
@@ -21,13 +21,13 @@ const navLinks = [
   },
 ];
 
-export default function Nav({user}) {
-  const [menuActive, setMenuActive] = useState(false)
+export default function Nav({ user }) {
+  const [menuActive, setMenuActive] = useState(false);
 
   return (
     <nav className="site-nav">
       <span className="menu-title">Blog</span>
-      <div className={`menu-content-container ${menuActive && 'active'}`}>
+      <div className={`menu-content-container ${menuActive && "active"}`}>
         <ul>
           {navLinks.map((link, index) => (
             <li key={index}>
@@ -42,8 +42,11 @@ export default function Nav({user}) {
           />
           <span className="menu-avatar-name">{`${user.firstName} ${user.lastName}`}</span>
         </span>
-        <i className = "ionicons icon ion-ios-menu" onClick={() => setMenuActive(!menuActive)}/>
       </div>
+      <i
+        className="ionicons icon ion-ios-menu"
+        onClick={() => setMenuActive(!menuActive)}
+      />
     </nav>
   );
 }
