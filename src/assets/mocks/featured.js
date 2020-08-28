fd2ab54bfc52a82315ef6e092d84ce99f1ae718e
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-08-28 15:15:13 -0700

    Saved images for blog posts

---
+++ src/assets/mocks/featured.js
@@ -0,0 +1,46 @@
+import moment from 'moment'
+
+export default [
+    {
+        title: 'Breastfeeding',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Breastfeeding', 'Other'],
+        link: 'a',
+        image: 'breastfeeding.jpg'
+    },
+    {
+        title: 'Kids',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Breastfeeding', 'Other'],
+        link: 'a',
+        image: 'kidbeach.jpg'
+    },
+    {
+        title: 'Apple',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Breastfeeding', 'Other'],
+        link: 'a',
+        image: 'apples.jpg'
+    },
+    {
+        title: 'Money',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Breastfeeding', 'Other'],
+        link: 'a',
+        image: 'money.jpg'
+    },
+    {
+        title: 'Sleeping',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Breastfeeding', 'Other'],
+        link: 'a',
+        image: 'babysleep.jpg'
+    },
+    {
+        title: 'Positive Thinking',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Breastfeeding', 'Other'],
+        link: 'a',
+        image: 'smile.jpg'
+    }
+]
