a2f914e4c5d0c5dd146939d9a4af90b48561626f
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-08-29 15:10:22 -0700

    MasonryPost

--- src/Components/common/MasonryPost.js
+++ src/Components/common/MasonryPost.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function MasonryPost( {post, tagsOnTop} ) {
-    const style={backgroundImage: `url("${require(`../../assets/img/${post.image}`)}")`};
+export default function MasonryPost({ post, tagsOnTop }) {
+  const style = {
+    backgroundImage: `url("${require(`../../assets/img/${post.image}`)}")`,
+  };
 
-    return (
-        <a className="masonry-post overlay" style={style} href={post.link}>
-            <div classname="image-text">
-                <div>
-                    <h2 classname="image-title">
-                        {post.title}
-                    </h2>
-                </div>
-            </div>
-        </a>
-    )
+  return (
+    <a className="masonry-post overlay" style={style} href={post.link}>
+      <div classname="image-text">
+        <div className="tags-container">
+            {post.catergories.map((tag, ind) => <span key={index} className="tag" style={{backgroundColor: catergoryColors(tag)}}></span>)}
+          <div>
+            <h2 classname="image-title">{post.title}</h2>
+            <span className="image-date">{post.date}</span>
+          </div>
+        </div>
+      </div>
+    </a>
+  );
 }
