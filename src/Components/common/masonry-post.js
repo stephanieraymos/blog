d44c7d886d775224cbcc6cedbb344654b5e50cee
Stephanie Raymos <stephanieraymos@gmail.com>
2020-08-30 13:40:44 -0700

    Building out catergories

--- src/Components/common/masonry-post.js
+++ src/Components/common/masonry-post.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { catergoryColors } from './styles';
+import TagRow from './'
 
 export default function MasonryPost({ post, tagsOnTop }) {
   const windowWidth = window.innerWidth
@@ -13,13 +13,7 @@ export default function MasonryPost({ post, tagsOnTop }) {
   return (
     <a className="masonry-post overlay" style={style} href={post.link}>
       <div classname="image-text" style={{ justifyContent: tagsOnTop ? 'space-between' : 'flex-end' }}>
-        <div className="tags-container">
-          {post.catergories.map((tag, ind) =>
-            <span key={ind} className="tag" style={{ backgroundColor: catergoryColors[tag] }}>
-              {tag.toUpperCase()}
-            </span>
-          )}
-        </div>
+        <TagRow tags={post.catergories} />
         <div>
           <h2 classname="image-title">{post.title}</h2>
           <span className="image-date">{post.date}</span>
