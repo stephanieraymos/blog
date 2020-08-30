36fd5e5b1aea22f7b50311f915259171a8a3ee1d
Stephanie Raymos <stephanieraymos@gmail.com>
2020-08-29 20:31:33 -0700

    Edited orange

--- src/Components/common/styles.js
+++ src/Components/common/styles.js
@@ -4,7 +4,7 @@ const catergoryColors = {
     'Food': 'rgb(255,45,85)',
     'Money': 'rgb(52,199,89)',
     'Sleeping': 'rgb(64,156,255)',
-    'Positive Thinking': 'rgb(255,179,64)',
+    'Positive Thinking': 'rgb(255,149,0)',
     'Other': 'rgb(88, 86, 214)',
     'To Be Decided': 'rgb(255,204,0)'
 
