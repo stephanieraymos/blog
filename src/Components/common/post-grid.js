c7363ce91202a54dfd01afbdce8617475bef6ea6
Stephanie Raymos <stephanieraymos@gmail.com>
2020-08-30 13:25:26 -0700

    Setting current post

--- src/Components/common/post-grid.js
+++ src/Components/common/post-grid.js
@@ -1,16 +1,26 @@
-import React, {useState, useMemo, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import {Pagination} from 'antd'
+import React, { useState, useMemo, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Pagination } from 'antd'
 
-export default function PostGrid({posts}) {
-const [pageSize, setPageSize] = useState(9)
+export default function PostGrid({ posts }) {
+  const [pageSize, setPageSize] = useState
+  const [current, setCurrent] = useState(1)
 
   return (
     <section className="grid-pagination-container">
-          <section className="post-grid container">
+      <section className="post-grid container">
 
-          </section>\
-          <Pagination simple showSizeChanger onShowSizeChange onShowSizeChange={setPageSize} pageSize={pageSize} />
+      </section>\
+      <Pagination
+        simple
+        showSizeChanger
+        onShowSizeChange
+        onShowSizeChange={setPageSize}
+        pageSize={pageSize}
+        total = {posts.length}
+        defaultCurrent = {current}
+        onChange = {setCurrent}
+      />
     </section>
   )
 }
