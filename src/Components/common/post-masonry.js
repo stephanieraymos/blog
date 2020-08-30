0b88d76cb04603a8be6532677140bb532bbc1183
Stephanie Raymos <stephanieraymos@gmail.com>
2020-08-29 20:16:08 -0700

    Fixed column error

--- src/Components/common/post-masonry.js
+++ src/Components/common/post-masonry.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {MasonryPost} from '.'
+import { MasonryPost } from './'
 
 export default function PostMasonry({posts, columns, tagsOnTop}) {
     return (
-        <section className="masonry" style={{gridTemplateColumns: `repeat(${columns}, min<max(275px, 1fr))`}}>
+        <section className="masonry" style={{gridTemplateColumns: `repeat(${columns}, minmax(275px, 1fr))`}}>
            { posts.map((post, index) =>
             <MasonryPost {...{post, index, tagsOnTop, key: index}} />
            )} 
