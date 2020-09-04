23eb1fd0ba2b9b9bb72d8e2d1767473af80232b2
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-09-03 22:13:49 -0700

    Mocks inside backend

---
+++ graphql/schemas/posts/mocks/trending.js
@@ -0,0 +1,60 @@
+import moment from 'moment'
+
+export default [
+    {
+        title: 'Breastfeeding',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Breastfeeding', 'Other'],
+        link: 'a',
+        image: 'breastfeeding.jpg'
+    },
+    {
+        title: 'Kids',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Other'],
+        link: '#',
+        image: 'kidbeach.jpg'
+    },
+    {
+        title: 'Apple',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Food', 'Other'],
+        link: '#',
+        image: 'apples.jpg'
+    },
+    {
+        title: 'Money',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Money', 'Other'],
+        link: '#',
+        image: 'money.jpg'
+    },
+    {
+        title: 'Sleeping',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Sleeping', 'Other'],
+        link: '#',
+        image: 'babysleep.jpg'
+    },
+    {
+        title: 'Positive Thinking',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Positive Thinking', 'Other'],
+        link: '#',
+        image: 'smile.jpg'
+    },
+    {
+        title: 'Potty Training',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Other'],
+        link: '#',
+        image: 'laughkid.jpg'
+    },
+    {
+        title: 'How to Save Money',
+        date: moment().format('MMMM DD, YYYY'),
+        catergories: ['Money', 'Other'],
+        link: '#',
+        image: 'friends.jpeg'
+    }
+]
