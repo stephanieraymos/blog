ace2f25c2b06bfda0865cc590c85c823aed23a30
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-09-03 22:20:27 -0700

    Resolvers

--- graphql/schemas/posts/index.js
+++ graphql/schemas/posts/index.js
@@ -2,3 +2,18 @@ const trendingPosts = require('./mocks/trending')
 const featuredPosts = require('./mocks/featured')
 const fs = require('fs')
 const path = require('path')
+const trending = require('./mocks/trending')
+
+module.exports = {
+    resolvers: {
+        Query: {
+            trendingPosts: ()  => trendingPosts,
+            featuredPosts: () => featuredPosts,
+            recentPosts: () => [
+                ...trendingPosts,
+                ...featuredPosts,
+                ...featuredPosts
+            ]
+        }
+    }
+}
