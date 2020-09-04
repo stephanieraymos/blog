b549b4ed367b54e09729423b0ce252d3d8d88b2a
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-09-03 22:27:01 -0700

    Returning file buffer

--- graphql/schemas/posts/index.js
+++ graphql/schemas/posts/index.js
@@ -1,19 +1,22 @@
-const trendingPosts = require('./mocks/trending')
-const featuredPosts = require('./mocks/featured')
-const fs = require('fs')
-const path = require('path')
-const trending = require('./mocks/trending')
+const trendingPosts = require("./mocks/trending");
+const featuredPosts = require("./mocks/featured");
+const fs = require("fs");
+const path = require("path");
+const trending = require("./mocks/trending");
 
 module.exports = {
-    resolvers: {
-        Query: {
-            trendingPosts: ()  => trendingPosts,
-            featuredPosts: () => featuredPosts,
-            recentPosts: () => [
-                ...trendingPosts,
-                ...featuredPosts,
-                ...featuredPosts
-            ]
-        }
-    }
-}
+  resolvers: {
+    Query: {
+      trendingPosts: () => trendingPosts,
+      featuredPosts: () => featuredPosts,
+      recentPosts: () => [...trendingPosts, ...featuredPosts, ...featuredPosts],
+    },
+  },
+  schema: fs
+    .readFileSync( //Returns file buffer.
+      path.resolver(
+        __dirname, //Current path that we're in. (Finds absolute path.)
+        "./posts-schema.graphql"
+      )
+    ).toString(),
+};
