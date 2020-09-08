cd11908434166115bd14e108e8424a2f08254d0d
Stephanie Raymos <stephanieraymos@gmail.com>
2020-09-07 19:17:51 -0700

    Connection config

---
+++ graphql/database/mysql.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+let config = {
+  client: 'mysql2',
+  connection: {
+    host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_USER,
+    database:process.env.MYSQL_PASS,
+    password:process.env.MYSQL_DB,
+    multipleStatements: true
+  }
+}
